20a3987f6a2257a01365c4b7d9208f69fdb7431a
Frederico Larangeira <fredericolarangeira@hotmail.com>
2026-07-19 22:08:28 -0300

    fixed tamed damage and resistance

--- src/main/java/com/github/bred_and_butter/attributes/EventsHandler.java
+++ src/main/java/com/github/bred_and_butter/attributes/EventsHandler.java
@@ -1,20 +1,28 @@
 package com.github.bred_and_butter.attributes;
 
 import com.github.bred_and_butter.SimpleAttributes;
+import com.mojang.logging.LogUtils;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
 
 @Mod.EventBusSubscriber(modid = SimpleAttributes.MODID)
 public class EventsHandler {
-    @SubscribeEvent
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    /*@SubscribeEvent(priority = EventPriority.LOW)
     public static void onLivingAttack(LivingAttackEvent event) {
+        LOGGER.info("entered on attack event");
+
         DamageSource source = event.getSource();
         // Check if the attacker is a tamed creature whose owner is a player
         if (source.getEntity() instanceof TamableAnimal tamableAnimal && tamableAnimal.getOwner() instanceof Player owner) {
@@ -29,7 +37,7 @@ public class EventsHandler {
         }
     }
 
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOW)
     public static void onLivingHurt(LivingHurtEvent event) {
         LivingEntity entity = event.getEntity();
         // Check if the hurt entity is a tamed creature owned by a player
@@ -41,6 +49,32 @@ public class EventsHandler {
                 event.setAmount(newDamage);
             }
         }
-    }
+    }*/
 
+
+    @SubscribeEvent
+    public static void onLivingHurt(LivingHurtEvent event) {
+        // 1. Tamed Damage – bonus damage dealt by tamed mobs
+        DamageSource source = event.getSource();
+        if (source.getEntity() instanceof TamableAnimal attacker
+                && attacker.isTame()
+                && attacker.getOwner() instanceof Player owner) {
+
+            double bonus = owner.getAttributeValue(AttributeRegister.TAMED_DAMAGE.get());
+            if (bonus > 0) {
+                event.setAmount((float) (event.getAmount() * (1 + (bonus/100))));
+            }
+        }
+
+        // 2. Tamed Resistance – damage reduction for tamed mobs
+        if (event.getEntity() instanceof TamableAnimal victim
+                && victim.isTame()
+                && victim.getOwner() instanceof Player owner) {
+
+            double reduction = owner.getAttributeValue(AttributeRegister.TAMED_RESISTANCE.get());
+            if (reduction > 0) {
+                event.setAmount(Math.max(1, (float) (event.getAmount() * (1 - (reduction/100)))));
+            }
+        }
+    }
 }
