e0241e675c575a218a88f872917c446687fe66ef
Frederico Larangeira <fredericolarangeira@hotmail.com>
2026-08-12 12:14:10 -0300

    added textures

--- src/main/java/com/github/bred_and_butter/effects/EffectRegister.java
+++ src/main/java/com/github/bred_and_butter/effects/EffectRegister.java
@@ -15,7 +15,7 @@ public class EffectRegister {
             "broken_shield", () -> new BrokenShieldEffect(MobEffectCategory.HARMFUL, 0x929c9b));
 
     public static final RegistryObject<MobEffect> BONUS_SHIELD = MOB_EFFECTS.register(
-            "bonus_shield", () -> new BonusShieldEffect(MobEffectCategory.BENEFICIAL, 0x00FFFF));
+            "shield_boost", () -> new ShieldBoostEffect(MobEffectCategory.BENEFICIAL, 0x00FFFF));
 
     public static final RegistryObject<MobEffect> FAST_REGEN = MOB_EFFECTS.register(
             "fast_regen", () -> new FastRegenEffect(MobEffectCategory.BENEFICIAL, 0x00FFFF));
